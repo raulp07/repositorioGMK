e05ed61ec753b765e671df55c38f4185f61ac777
raulp07 <sefiroth1990@gmail.com>
2017-09-15 18:14:20 -0500

    nuevo modulo

---
+++ Web/SISGFRAN/UPC.SISGFRAN.Web/Controllers/SugerirTemporadaPromocionController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UPC.SISGFRAN.Web.Controllers
+{
+    public class SugerirTemporadaPromocionController : Controller
+    {
+        //
+        // GET: /SugerirTemporadaPromocion/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
